33391d4f6c0d1d7e8a320e596b5c4eb413ca124a
Bird <bird@BIRD-MBP.local>
2020-02-15 11:36:04 +0700

    refacetor TestFizzBuzz1To100

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -3,5 +3,18 @@ package fizzbuzz
 import "strconv"
 
 func Say(n int) string {
+	if n%3 == 0 && n%5 == 0 {
+		return "FizzBuzz"
+	}
+	if n%3 == 0 {
+		return "Fizz"
+	}
+	if n%5 == 0 {
+		return "Buzz"
+	}
 	return strconv.Itoa(n)
 }
+
+// 3 f
+// 5 b
+// 3 5 fb
