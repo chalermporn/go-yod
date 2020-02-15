b0e4d7beff22025df2d88304eec888cab9d2ff72
Bird <bird@BIRD-MBP.local>
2020-02-15 12:19:52 +0700

    rest api

--- fizzbuzz/fizzbuzz_integration_test.go
+++ fizzbuzz/fizzbuzz_integration_test.go
@@ -4,13 +4,18 @@ package fizzbuzz_test
 
 import (
 	"fmt"
-	"hello/fizzbuzz"
+	"hello/fizzbuzz" // name: hello from go.mod,  directory: fizzbuzz
 	"testing"
 )
 
 func TestFizzBuzz1To100(t *testing.T) {
 	for i := 1; i <= 100; i++ {
-		fmt.Print(fizzbuzz.Say(i) + ",")
+		fmt.Print(fizzbuzz.Say(i) + ", ") // use packet fizzbuzz.go
 	}
 
 }
+
+// คำแนะนำ
+// ตั้งชืื่อ ตาม packet กับ directory ให้เหมือนกัน
+// ชื่อ  directory ควรเป็นตัวเล็กหมด
+// func ควรเป็นตัวใหญ่
--- main.go
+++ main.go
@@ -1,19 +1,39 @@
 package main
 
 import (
-	"fmt"
+	"hello/fizzbuzz"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
-var name string
-var fname = "Chalermporn"
-
 func main() {
-	fmt.Println("Hello go")
+	// Hello world, the web server
+
+	// helloHandler := func(w http.ResponseWriter, req *http.Request) {
+	// 	io.WriteString(w, "Hello, world!\n")
+	// }
+	r := mux.NewRouter()
+
+	r.HandleFunc("/fizzbuzz/{number}", fizzbuzzHandler)
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+func fizzbuzzHandler(w http.ResponseWriter, req *http.Request) {
+	// vars := mux.Vars(req)
+	// numberString := vars["number"]
+	n, _ := strconv.Atoi(mux.Vars(req)["number"])
+	io.WriteString(w, fizzbuzz.Say(n))
 }
 
-func add(a int, b int) int {
-	return a + b
-}
-func remove(a, b int) int {
-	return a - b
-}
+// func add(a int, b int) int {
+// 	return a + b
+// }
+// func remove(a, b int) int {
+// 	return a - b
+// }
+
+// mecho
+// go datebase-sql
