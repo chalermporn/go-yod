74a29269b7418c629db38bef7b5ded83c5284e81
Bird <bird@BIRD-MBP.local>
2020-02-15 10:54:58 +0700

    first mian

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+var name string
+var fname = "Chalermporn"
+
+func main() {
+	fmt.Println("Hello go")
+}
+
+func add(a int, b int) int {
+	return a + b
+}
+func remove(a, b int) int {
+	return a - b
+}
